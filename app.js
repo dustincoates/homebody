4d771c070837fa95b9bad9e1e7d0e9540889aaa5
Dustin Coates <dustin@dcoates.com>
2013-09-14 15:33:45 -0400

    Use Stylus

--- app.js
+++ app.js
@@ -5,9 +5,18 @@
 
 var express = require('express')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , stylus = require('stylus')
+  , nib = require('nib');
 
 var app = express();
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -15,6 +24,12 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(stylus.middleware(
+  { src: __dirname + '/public/stylus'
+  , dest: __dirname + '/public'
+  , compile: compile
+  }
+));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
