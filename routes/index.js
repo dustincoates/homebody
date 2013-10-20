0739206e48866df755bbdaadb9c050171f89ec2e
Dustin Coates <dustin@dcoates.com>
2013-10-20 09:58:19 -0400

    Moved socket out of the route handler

--- routes/index.js
+++ routes/index.js
@@ -16,21 +16,24 @@ app.get('/data/mta/:lines', function(req, res) {
 
 app.get('/hello', function(req, res) {
   res.render('hello', { title: 'Express' });
-  // need to clean this up for each info type
-  io.sockets.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('add note', function(data) {
-      note = new db.Note();
-      note.message = data.message;
-      note.active = true;
-      note.dateTimeCreated = new Date();
-      note.save(function(err) {
-        if(err){
-          socket.emit('news', { note: err })
-        } else {
-          socket.emit('news', { note: 'Note saved' })
-        }
-      });
+});
+
+// need to clean this up for each info type
+// Should probably move this to a socket router file
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('add note', function(data) {
+    note = new db.Note();
+    note.message = data.message;
+    note.active = true;
+    note.dateTimeCreated = new Date();
+    note.save(function(err) {
+      if(err){
+        socket.emit('news', { note: err })
+      } else {
+        console.log("Note saved");
+        socket.emit('news', { note: 'Note saved' })
+      }
     });
   });
 });
